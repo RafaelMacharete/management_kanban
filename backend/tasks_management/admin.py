9b8fc6919d8dafc06861c691360a469323ff55f2
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-13 16:59:39 -0300

    feat: relation working
    to fix many to many reference

--- backend/tasks_management/admin.py
+++ backend/tasks_management/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(account)
-# admin.site.register(Board)
-# admin.site.register(Column)
-# admin.site.register(Task)
-# admin.site.register(Project)
+admin.site.register(Account)
+admin.site.register(Project)
+admin.site.register(Board)
+admin.site.register(Column)
+admin.site.register(Task)
--- backend/tasks_management/models.py
+++ backend/tasks_management/models.py
@@ -4,19 +4,41 @@ from django.db import models
 
 class Project(models.Model):
     project_name = models.CharField(max_length=50)  
-    owner = models.CharField(max_length=50)
-    contributors = models.CharField()
+    owner = models.ForeignKey('Account', on_delete=models.CASCADE)
+    contributors = models.ForeignKey('Account', on_delete=models.CASCADE, related_name='AccountContributor')
 
+    def __str__(self): 
+        return self.project_name
 
 class Account(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=30)
-    projects = models.ManyToManyField(Project, through=AccountInProject)
+    projects = models.ManyToManyField('AccountContributor', blank=True)
 
     def __str__(self):
         return self.username
-    
-class AccountInProject(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE) 
+
+class Board(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.project_name.project_name
+
+class Column(models.Model):
+    column_name = models.CharField(max_length=30)
+    board_name = models.ForeignKey(Board, on_delete=models.CASCADE)
+    order_position = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.column_name
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=50)
+    description = models.TextField()
+    creation_date = models.DateField(auto_created=True)
+    updated_date = models.DateField(auto_now=True)
+    column_name = models.ForeignKey(Column, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task_name
