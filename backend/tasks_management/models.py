d16eea81a2186802cf76398c61704f95c2ead434
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-14 17:02:42 -0300

    remoduling database

--- backend/tasks_management/models.py
+++ backend/tasks_management/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     project_name = models.CharField(max_length=50)  
-
+    
     def __str__(self): 
         return self.project_name
 
@@ -11,21 +11,20 @@ class Account(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=30)
-    projects = models.ManyToManyField(
+    '''projects = models.ManyToManyField(
         Project, 
         through='AccountProjects',
         through_fields=('contributors', 'owner'),
         blank=True
-    )
+    )'''
 
     def __str__(self):
         return self.username
-
+'''
 class AccountProjects(models.Model):
     owner = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='owner_project')
     contributors = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='contributors_project')
-
-
+'''
 class Board(models.Model):
     project_name = models.ForeignKey(Project, on_delete=models.CASCADE)
 
