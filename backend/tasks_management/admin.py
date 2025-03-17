f8797a7964721cb05131bed1506c95a35596069e
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-17 17:02:28 -0300

    feat: fixing models

--- backend/tasks_management/admin.py
+++ backend/tasks_management/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Account)
-admin.site.register(Project)
-admin.site.register(Board)
+admin.site.register(ProjectBoard)
 admin.site.register(Column)
-admin.site.register(Task)
+admin.site.register(Card)
--- backend/tasks_management/models.py
+++ backend/tasks_management/models.py
@@ -1,50 +1,39 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.db import models
 
-class Project(models.Model):
-    project_name = models.CharField(max_length=50)  
-    
-    def __str__(self): 
-        return self.project_name
-
 class Account(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, primary_key=True)
+    password = models.CharField(max_length=40)
     email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=30)
-    '''projects = models.ManyToManyField(
-        Project, 
-        through='AccountProjects',
-        through_fields=('contributors', 'owner'),
-        blank=True
-    )'''
-
+    role = models.CharField(max_length=11)
+ 
     def __str__(self):
         return self.username
-'''
-class AccountProjects(models.Model):
-    owner = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='owner_project')
-    contributors = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='contributors_project')
-'''
-class Board(models.Model):
-    project_name = models.ForeignKey(Project, on_delete=models.CASCADE)
 
+class ProjectBoard(models.Model):
+    project_board_name = models.CharField(max_length=50, primary_key=True)
+    projects = models.ManyToManyField(Account, through='ProjectBoardAccount')
+    
     def __str__(self):
-        return self.project_name.project_name
+        return self.project_board_name
+
+class ProjectBoardAccount(models.Model):
+    account_username = models.ForeignKey(Account, on_delete=models.CASCADE)
+    project_board_name = models.ForeignKey(ProjectBoard, on_delete=models.CASCADE)
 
 class Column(models.Model):
-    column_name = models.CharField(max_length=30)
-    board_name = models.ForeignKey(Board, on_delete=models.CASCADE)
-    order_position = models.IntegerField(default=1)
-
+    column_name = models.CharField(max_length=30, primary_key=True)
+    project_name = models.ForeignKey(ProjectBoard, on_delete=models.CASCADE)
+    position = models.IntegerField()
+    
     def __str__(self):
         return self.column_name
 
-class Task(models.Model):
-    task_name = models.CharField(max_length=50)
-    description = models.TextField()
-    creation_date = models.DateField(auto_created=True)
-    updated_date = models.DateField(auto_now=True)
-    column_name = models.ForeignKey(Column, on_delete=models.CASCADE)
-
+class Card(models.Model):
+    card_name = models.CharField(max_length=35, primary_key=True)
+    column_name = models.ForeignKey(Column, on_delete=models.CASCADE) 
+    card_description = models.TextField()
+    card_creation_update_date = models.DateField(auto_now_add=1)
+    
     def __str__(self):
-        return self.task_name
+        return self.card_name
