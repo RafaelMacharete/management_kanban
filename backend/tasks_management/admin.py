c652920000e74ae500dd2e78ebeac4d60581036e
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-28 13:44:43 -0300

    fixing: admin register

--- backend/tasks_management/admin.py
+++ backend/tasks_management/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
-admin.site.register(Account)
-admin.site.register(ProjectBoard)
-admin.site.register(Column)
-admin.site.register(Card)
-
 class AccountAdmin(UserAdmin):
-    list_display = ('username', 'email', 'username', 'is_staff')
+    list_display = ('username', 'email', 'username', 'is_staff', 'phone_number')
     fieldsets = UserAdmin.fieldsets + (
         (None, 
-         {'fields' : ('username')}),
+         {'fields' : ('phone_number',)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, 
-         {'fields' : ('username')}),
+         {'fields' : ('phone_number',)}),
     )
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(ProjectBoard)
+admin.site.register(Column)
+admin.site.register(Card)
