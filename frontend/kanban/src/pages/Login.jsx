9e7be857fa339b8bd77d7daf99d4c2dcd9ca7865
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-04-04 10:48:08 -0300

    feat: redirecting to home after login

--- frontend/kanban/src/pages/Login.jsx
+++ frontend/kanban/src/pages/Login.jsx
@@ -1,90 +1,86 @@
-import { Navbar } from '../components/Navbar'
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 export function Login() {
-    const [formData, setFormData] = useState({
-        username: "",
-        password: "",
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [navigate, setNavigate] = useState(false);
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await fetch("http://127.0.0.1:8000/login/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: 'include',
+      body: JSON.stringify(formData),
     });
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-    function handleChange(e) {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+    const body = await response.json();
+    if (body.access) {
+      setNavigate(true);
     }
+  }
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const response = await fetch('http://127.0.0.1:8000/login/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        })
-        const body = await response.json()
-        alert(body)
-    }
-
-    return (
-        <>
-            <div
-                className="flex items-center justify-center min-h-screen">
-                <form
-                    className="bg-white p-6 rounded-2xl shadow-indigo-500 shadow-lg w-80"
-                    onChange={handleChange}
-                    onSubmit={handleSubmit}
-                    method='POST' // Esconde os campos do formulário
-                >
-                    <h2
-                        className="text-2xl font-semibold text-[#5030E5] text-center mb-4"
-                    >
-                        Login
-                    </h2>
-                    <div
-                        className="mb-4">
-                        <label
-                            htmlFor="username"
-                            className="block text-[#787486] mb-1"
-                        >
-                            Username
-                        </label>
-                        <input
-                            type="text"
-                            name="username"
-                            id="username"
-
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#5030E5]"
-                        />
-                    </div>
-                    <div
-                        className="mb-4">
-                        <label htmlFor="password"
-                            className="block text-[#787486] mb-1"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#5030E5]"
-                        />
-                    </div>
-                    <div
-                        className="flex justify-between items-center mb-4">
-                        <Link to="/register"
-                            className="text-[#5030E5] text-sm hover:underline"
-                        >
-                            Register
-                        </Link>
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-[#5030E5] text-white py-2 rounded-lg hover:bg-[#4025b8] transition">
-                        Login
-                    </button>
-                </form>
-            </div>
-        </>
-    );
+  if (navigate) {
+    return <Navigate to="/projects" />;
+  }
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <form
+          className="bg-white p-6 rounded-2xl shadow-indigo-500 shadow-lg w-80"
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          method="POST" // Esconde os campos do formulário
+        >
+          <h2 className="text-2xl font-semibold text-[#5030E5] text-center mb-4">
+            Login
+          </h2>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-[#787486] mb-1">
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#5030E5]"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-[#787486] mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#5030E5]"
+            />
+          </div>
+          <div className="flex justify-between items-center mb-4">
+            <Link
+              to="/register"
+              className="text-[#5030E5] text-sm hover:underline"
+            >
+              Register
+            </Link>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-[#5030E5] text-white py-2 rounded-lg hover:bg-[#4025b8] transition"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
