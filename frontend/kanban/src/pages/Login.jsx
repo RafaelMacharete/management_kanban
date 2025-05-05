05f45cbb6ec4f9441ae91fb2ae30b178b121caaa
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-05-05 10:10:01 -0300

    trying to: updating url fetching on front

--- frontend/kanban/src/pages/Login.jsx
+++ frontend/kanban/src/pages/Login.jsx
@@ -23,7 +23,7 @@ export function Login() {
 
         setIsLoading(true);
 
-        const response = await fetch("http://127.0.0.1:8000/login/", {
+        const response = await fetch("http://https://trellio.onrender.com/login/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             credentials: 'include',
