9f740a6d0cbc58d16f54b54b8085020390b35e5e
Macharete_r <rafaelmacharete00@gmail.com>
2025-07-07 16:54:21 -0300

    feat: try to upload images:

--- backend/kanban/urls.py
+++ backend/kanban/urls.py
@@ -3,11 +3,21 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import re_path
+from django.views.static import serve as media_serve
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks_management.urls'))
 ]
 urlpatterns += staticfiles_urlpatterns()
 
-# if settings.DEBUG:
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', media_serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
